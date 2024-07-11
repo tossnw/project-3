7e0c79a321d81cd35dc7d9f89cd1dec25e37a3e9
toss <reactdeveloper82@gmail.com>
2024-07-11 14:51:41 +0500

    v-2.2 fix burger menu scroll problem

--- scripts/burgerMenu.js
+++ scripts/burgerMenu.js
@@ -2,13 +2,17 @@ var burgerMenu = document.getElementById("burger-menu");
 var overlay = document.getElementById("menu");
 const header = document.querySelector("header");
 const winHeight = window.screen.height;
-const body = document.body
+const body = document.body;
 console.log(body);
 header.style.height = `${winHeight}px`;
 
 burgerMenu.addEventListener("click", function () {
-	body.style.overflowY = 'hidden'
-	this.classList.toggle("close");
-	overlay.classList.toggle("overlay");
-  });
-  
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+
+  if (this.classList.value === "close") {
+    body.style.overflowY = "hidden";
+  } else {
+    body.style.overflowY = "scroll";
+  }
+});
